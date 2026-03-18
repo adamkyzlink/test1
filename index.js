36663ec581e85422f3b7d695d10f80ee6379267f
Adam Kyzlink <adam.kyzlink@gmail.com>
2026-03-18 20:02:05 +0100

    added index

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./api/index.js')
+const express = require("express")
+const { getResults } = require("./api/scraper")
+
+const app = express()
+app.use(express.static("public"))
+
+app.get("/search", async (req, res) => {
+    const query = req.query.q
+    const results = await getResults(query)
+    res.json(results)
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+module.exports = app
